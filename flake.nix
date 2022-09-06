6e549df8c8b2379c6d2f8e7d66086457b59cfb0c
Douglas Mayle <dougle@google.com>
2022-09-06 10:44:12 -0700

    Initial flake import

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "dmayle's nix system configurations, both nixos and home-manager";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+  };
+}

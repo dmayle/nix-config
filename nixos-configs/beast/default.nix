e9d2e777c4cf77e8023464c8af4631583606cdd4
Douglas Mayle <douglas@mayle.org>
2022-10-07 21:54:38 +0000

    Add basic config for the beast

--- nixos-configs/beast/default.nix
+++ nixos-configs/beast/default.nix
@@ -1,4 +1,4 @@
-{ config, inputs, ... }:
+{ pkgs, config, inputs, lib, ... }:
 
 {
   imports = [
@@ -11,4 +11,41 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   time.timeZone = "Americas/Los_Angeles";
+  networking.hostName = "beast";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+  services.xserver.layout = "gb";
+  services.xserver.xkbOptions = "caps:swapescape";
+  services.xserver.xkbVariant = "extd";
+  services.xserver.xkbModel = "pc105";
+  services.xserver.libinput.enable = true;
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  nix.settings.allowed-users = [ "@wheel" ];
+
+  users.users.douglas = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "sway" "video" "i2c" ];
+  };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+  };
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+  ];
+  environment.etc.inputrc.text = lib.mkAfter ''
+    set editing-mode vi
+    set keymap vi
+  '';
+  environment.variables.EDITOR = "nvim";
+  services.openssh.enable = true;
+  system.stateVersion = "22.05"; # Did you read the comment?
 }

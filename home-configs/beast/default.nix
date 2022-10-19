3d6136d1b7fd782e182bb7a6ad5a722aeb37ae32
Douglas Mayle <dougle@google.com>
2022-10-18 21:35:52 -0700

    Ensure gnome doesn't go to sleep

--- home-configs/beast/default.nix
+++ home-configs/beast/default.nix
@@ -7,4 +7,13 @@
     homeManagerProfiles.sway
     homeManagerProfiles.tmux
   ];
+
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+      sleep-inactive-battery-type = "nothing";
+      sleep-inactive-ac-timeout = 0;
+      sleep-inactive-battery-timeout = 0;
+    };
+  };
 }

e0115edca8e028c56290c49145a5f5aaabe0007e
Douglas Mayle <dougle@google.com>
2023-03-11 18:20:39 -0800

    Temporary add joycond_cemuhook devShell until I package it

--- dev-shells/python_joycond_cemuhook/default.nix
+++ dev-shells/python_joycond_cemuhook/default.nix
@@ -6,4 +6,8 @@ let
       my-app = ./src;
     };
   };
-in pyAppEnv.env
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [ python39 poetry python39Packages.dbus-python python39Packages.evdev python39Packages.pyudev python39Packages.termcolor ];
+  }
+#in pyAppEnv.env

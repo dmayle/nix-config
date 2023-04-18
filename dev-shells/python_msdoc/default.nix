99b8ec8faac5cad8039c38a70b74e1e434357740
Douglas Mayle <dougle@google.com>
2023-04-17 18:31:19 -0700

    Now for realz this time

--- dev-shells/python_msdoc/default.nix
+++ dev-shells/python_msdoc/default.nix
@@ -2,6 +2,10 @@
 let
   my-packages = p: with p; [
     python-docx
+    libxml2
+    pkgs.libxml2
+    pkgs.libxml2Python
+    lxml
   ];
   my-python = pkgs.python3.withPackages my-packages;
 in my-python.env

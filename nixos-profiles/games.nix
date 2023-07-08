140da958f1996fb97bb3c3eda86d36af6b3cbf31
Douglas Mayle <douglas@mayle.org>
2023-07-07 18:47:13 -0700

    Remove joycond completely, I'm no longer using it

--- nixos-profiles/games.nix
+++ nixos-profiles/games.nix
@@ -2,9 +2,9 @@
   environment.systemPackages = with pkgs; [
     pstree #Test
   ];
-  #joycond is necessary for gyro with cemu, but prevents gyro in Steam
-  services.joycond.enable = true;
+
   hardware.steam-hardware.enable = true;
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;

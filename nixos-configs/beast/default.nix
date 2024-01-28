cf54ddffa34788567d5f6746fd4a6170790692fb
Douglas Mayle <douglas@mayle.org>
2024-01-28 10:25:08 -0800

    Rearrance and cleanup nixos config

--- nixos-configs/beast/default.nix
+++ nixos-configs/beast/default.nix
@@ -17,17 +17,23 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Add NTFS support
-  xdg.portal.enable = lib.mkForce false;
   boot.supportedFilesystems = [ "ntfs" ];
 
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  # Setup the machine name
   networking.hostName = "beast";
+
+  # Set your time zone and locale
+  time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
+
+  # Basic console configuration supporting latin languages
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true; # use xkbOptions in tty.
   };
+
+  # Setup the hardware to use nvidia drivers, and ensure hardware acceleration
+  # support for wayland
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.beta;
@@ -36,6 +42,7 @@
     modesetting.enable = true;
     nvidiaSettings = true;
   };
+
   hardware.opengl = {
     enable = true;
     driSupport = true;
@@ -44,14 +51,27 @@
       vaapiVdpau
       libvdpau-va-gl
       nvidia-vaapi-driver
+      vulkan-validation-layers
+      mesa.drivers
     ];
     extraPackages32 = with pkgs.pkgsi686Linux; [
       vaapiVdpau
       libvdpau-va-gl
       nvidia-vaapi-driver
+      vulkan-validation-layers
+      mesa.drivers
     ];
   };
+
+  # Nvidia fixup
+  services.xserver.deviceSection = ''
+    Option "SidebandSocketPath" "/tmp"
+  '';
+
+
+  # TODO: move this to sound profile
   hardware.pulseaudio.support32Bit = true;
+
   # Configure keymap in X11
   services.xserver.layout = "gb";
   services.xserver.xkbOptions = "caps:swapescape";
@@ -59,24 +79,34 @@
   services.xserver.xkbModel = "pc105";
   services.xserver.libinput.enable = true;
   services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.desktopManager.xterm.enable = true;
-  # Temporarily remove 8k because of issue booting with 530.41.03
-  #services.xserver.resolutions = [{ x = 7680; y = 4320; }{ x = 3840; y = 2160; }];
-  #services.xserver.resolutions = [{ x = 3840; y = 2160; }];
+
   services.xserver.resolutions = [{ x = 7680; y = 4320; }];
   services.xserver.displayManager.gdm = {
     enable = true;
     wayland = true;
     autoSuspend = false;
   };
+
+  # We enable desktop managers in order to setup a graphical login with gnome
+  # and sway. We want those to be configured by home-manager, so we'll have to
+  # fixup this config a bit
+  services.xserver.desktopManager.gnome.enable = true;
+  # This adds a sway session to gdm, but the session just selects sway from the
+  # current environment. My home manager config will override that version, so
+  # all config should be done there.
   programs.xwayland.enable = true;
-  #services.xserver.extraConfig = "";
-  #services.xserver.extraDisplaySettings = "";
-  #services.xserver.extraLayouts.foo = {};
-  services.xserver.deviceSection = ''
-    Option "SidebandSocketPath" "/tmp"
-  '';
+  programs.sway = {
+    enable = true;
+  };
+
+  # Enabling desktop managers forces xdg-desktop-portal (services for sandboxed
+  # applications) to be configured. We want to configure this at the
+  # home-manager level, so we forcibly disable it here.
+  xdg.portal.enable = lib.mkForce false;
+
+  # TODO: move this to desktop profile
+  # This is a desktop machine we don't want turning off so that we can access
+  # it from the network
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
@@ -87,6 +117,8 @@
     extraGroups = [ "wheel" "audio" "sway" "video" "i2c" "docker" "vboxusers" ];
   };
   nix.settings.allowed-users = [ "@wheel" ];
+
+  # Some basic packages I want available if I use the system as root
   environment.systemPackages = with pkgs; [
     xorg.libpciaccess
     wget
@@ -98,6 +130,8 @@
 
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 
+  # Very basic neovim config for root because vanilla vim is frustrating when
+  # unconfigured
   programs.neovim = {
     enable = true;
     viAlias = true;
@@ -105,31 +139,25 @@
     defaultEditor = true;
   };
 
+  environment.variables.EDITOR = "nvim";
+  environment.etc.inputrc.text = lib.mkAfter ''
+    set editing-mode vi
+    set keymap vi
+  '';
+
   # Ensure that nix-* (e.g. nix-shell) use the same nixpkgs as this flake
   environment.etc."nixpath/nixpkgs".source = inputs.nixpkgs;
   nix.nixPath = [
     "nixpkgs=/etc/nixpath/nixpkgs"
   ];
 
-  environment.etc.inputrc.text = lib.mkAfter ''
-    set editing-mode vi
-    set keymap vi
-  '';
-
-  environment.variables.EDITOR = "nvim";
-
-  # This adds a sway session to gdm, but the session just selects sway from the
-  # current environment. My home manager config will override that version, so
-  # all config should be done there.
-  programs.sway = {
-    enable = true;
-  };
-
+  # Enable remote access
   services.openssh = {
     enable = true;
     settings.X11Forwarding = true;
   };
 
+  # Prefer the network firewall
   networking.firewall.enable = false;
 
   system.stateVersion = "22.05"; # Did you read the comment?

f52b80874919a1145a1d757823bd732d776ea9b9
Douglas Mayle <douglas@mayle.org>
2024-03-07 18:10:44 -0800

    Prevent jupyter from launching browser (which it can, now that I import environment into systemd user sessions)

--- home-profiles/jupyterlab.nix
+++ home-profiles/jupyterlab.nix
@@ -42,7 +42,7 @@ in
     };
     Service = {
       Type = "simple";
-      ExecStart = "${myJupyter}/bin/jupyter-lab --ip=0.0.0.0";
+      ExecStart = "${myJupyter}/bin/jupyter-lab --ip=0.0.0.0 --no-browser";
       Restart = "always";
       RestartSec = 3;
     };

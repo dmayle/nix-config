3deece2addb9ae6c314e984121b3451ac53ed7c5
Douglas Mayle <douglas@mayle.org>
2024-06-23 18:21:15 -0700

    Fix Gitsigns config to match new version

--- home-profiles/neovim/init.lua
+++ home-profiles/neovim/init.lua
@@ -522,11 +522,11 @@ require('nvim-tree').setup({
 
 require('gitsigns').setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { text = "▎" },
+    change = { text = "▎" },
+    delete = { text = "▎" },
+    topdelete = { text = "▎" },
+    changedelete = { text = "▎" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
@@ -558,9 +558,6 @@ require('gitsigns').setup {
     relative = "cursor",
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
   },
   on_attach = function()
     local gitsigns = require('gitsigns')

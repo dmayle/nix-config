e87d9598da0f7093a077bed013ea1c1bff091801
Douglas Mayle <douglas@mayle.org>
2024-09-14 11:04:41 -0400

    Create a home-manager config for fox

---
+++ home-configs/fox/default.nix
@@ -0,0 +1,15 @@
+{ inputs, ... }:
+{
+  imports = with inputs.self; [
+    homeManagerRoles.virtual-machine
+    homeManagerProfiles.fonts
+    homeManagerProfiles.neovim
+    homeManagerProfiles.tmux
+  ];
+
+  home = rec {
+    username = "douglas";
+    stateVersion = "22.05";
+    homeDirectory = "/home/${username}";
+  };
+}

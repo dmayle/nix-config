450adc93eeaa4a67526c409c80778a9caeef5212
Douglas Mayle <douglas@mayle.org>
2025-08-22 15:45:03 -0400

    Now that whisper is up-to-date, remove override, add LM Studio

--- home-profiles/ai-gui.nix
+++ home-profiles/ai-gui.nix
@@ -1,16 +1,7 @@
 { pkgs, ... }:
-let
-  whisper176 = pkgs.whisper-cpp.overrideAttrs (oldAttrs: {
-    src = pkgs.fetchFromGitHub {
-      owner = "ggml-org";
-      repo = "whisper.cpp";
-      rev = "a8d002cfd879315632a579e73f0148d06959de36";
-      sha256 = "sha256-dppBhiCS4C3ELw/Ckx5W0KOMUvOHUiisdZvkS7gkxj4=";
-    };
-  });
-in
 {
-  home.packages = [
-    whisper176
+  home.packages = with pkgs; [
+    whisper-cpp
+    lmstudio
   ];
 }
